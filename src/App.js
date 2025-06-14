adbc4e38aa8458c085ff85b7a576b91ecc66140f
Charlescsa <charlescsa1@gmail.com>
2025-06-14 18:40:41 +0530

    App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontFamily: 'sans-serif' }}>
+      <h1>💫 Welcome Charles</h1>
+      <h2>This is SARAIX Eternal Mind GUI</h2>
+      <p>Deploy me on Vercel now!</p>
+    </div>
+  );
+}
+
+export default App;
